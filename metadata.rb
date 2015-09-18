b657e10da09b740be9a1b00b1442544e30266e06
Ryan Ovrevik <rovrevik@ovrevik.com>
2015-09-17 23:06:55 -0400

    execute chef generate cookbook docker_host

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'docker_host'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures docker_host'
+long_description 'Installs/Configures docker_host'
+version '0.1.0'
